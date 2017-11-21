49d17652ffc373eb0f0447634293cca63153c78a
affeyo <ah.langaas@gmail.com>
2017-11-21 14:35:52 +0100

    Made threads into function and runs it twice

--- Bazaar/Program.cs
+++ Bazaar/Program.cs
@@ -13,21 +13,22 @@ namespace Bazaar
 	{
 
         private static Random rand = new Random();
+        private static Thread[] threads = new Thread[20];
+        
 
-		static void Main(string[] args)
+        static void Main(string[] args)
 		{
-            Thread[] threads = new Thread[20];
-            List<Customer> customers = new List<Customer>();
+            
 			Store tcg = new Store();
+            List<Customer> customers = new List<Customer>();
+            /* What we want it to look like
+            tcg.AddItem("Swift Spectral Tiger", "Legendary", 75);
+            tcg.AddItem("Rare Card", "Rare", 15);
+            tcg.AddItem("Test", "Epic", 5);
+            */
 
-			/* What we want it to look like
-			tcg.AddItem("Swift Spectral Tiger", "Legendary", 75);
-			tcg.AddItem("Rare Card", "Rare", 15);
-			tcg.AddItem("Test", "Epic", 5);
-			*/
-
-			// Create items
-			StoreItem spectral = new StoreItem("Swift Spectral Tiger", "Legendary", 50);
+            // Create items
+            StoreItem spectral = new StoreItem("Swift Spectral Tiger", "Legendary", 50);
 			StoreItem test = new StoreItem("TCG", "Epic", 10);
 			StoreItem test2 = new StoreItem("Dildo", "Rare", 5);
 			spectral.printItemHelp();
@@ -37,7 +38,7 @@ namespace Bazaar
             // Create customers
             Customer lyband = new Customer("Lyband", 0);
             customers.Add(lyband);
-			Customer santom = new Customer("Santom", 100);
+			Customer santom = new Customer("Santom", 50);
             customers.Add(santom);
             Customer lanalf = new Customer("lanalf", 100);
             customers.Add(lanalf);
@@ -48,17 +49,11 @@ namespace Bazaar
             #region Multithread test
             // One transaction per person
             // Initializing transaction threads and starting.
-            for (int i = 0; i < shuffledCustomers.Count; ++i)
-            {
-                Thread t = new Thread(() => shuffledCustomers.ElementAt(i).BuyItem(spectral));
-                threads[i] = t;
 
-                // Used for debug
-                //Console.Write("Person: " + customers[i].CustomerName + " index: " + i + "\n");
-                threads[i].Start();
-                Thread.Sleep(1000);
-
-            }
+            // Every customer will try to buy spectral
+            DoTransactions(shuffledCustomers, spectral);
+            shuffledCustomers = ShuffleCustomerList(shuffledCustomers);
+            DoTransactions(shuffledCustomers, test);
 
             #endregion
 
@@ -79,6 +74,21 @@ namespace Bazaar
                 list.Remove(list[r]);
             }
             return result;
+        }
+
+        private static void DoTransactions(List<Customer> customers, StoreItem storeItem)
+        {
+            for (int i = 0; i < customers.Count; ++i)
+            {
+                Thread t = new Thread(() => customers.ElementAt(i).BuyItem(storeItem));
+                threads[i] = t;
+
+                // Used for debug
+                //Console.Write("Person: " + customers[i].CustomerName + " index: " + i + "\n");
+                threads[i].Start();
+                Thread.Sleep(1000);
+
+            }
         }
 	}
 }
