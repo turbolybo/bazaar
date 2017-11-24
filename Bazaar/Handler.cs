ec3f61b93d036681d3051661172dd1ec35acea4f
turbolybo <andreas_lybo@hotmail.com>
2017-11-24 13:39:20 +0100

    Handler finished

--- Bazaar/Handler.cs
+++ Bazaar/Handler.cs
@@ -1,11 +1,6 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Bazaar
 {
@@ -23,41 +18,68 @@ namespace Bazaar
 		{
 			// Adding customers to list
 			customers.Add(new Customer("Santom", 100));
-			customers.Add(new Customer("Lyband", 1000));
-			customers.Add(new Customer("lanalf", 1000));
+			customers.Add(new Customer("Lyband", 50));
+			customers.Add(new Customer("Lanalf", 75));
 		}
+
+		/// <summary>
+		/// Start method. Program will not start shop sales until this is called. 
+		/// </summary>
+		#region Start
 		public void Start()
 		{
 			MultiThreading();
 			Sleep();
 		}
+		#endregion
+
+		/// <summary>
+		/// Using multithreading to make sure only one customer can buy one item.
+		/// Sleeping between stores putting items up for sale.
+		/// </summary>
+		#region MultiThreading
 		public void MultiThreading()
 		{ 
             // Initializing and starting transaction threads for TCG shop
             for (int i = 0; i < 10; i++)
             {
+				// TCG shop
                 Console.Write("\n");
                 stores.tcg.generateItems(stores.tcg.storeItems, 1);
-                Thread.Sleep(1000);
+                Thread.Sleep(50);
                 DoTransactions(customers, stores.tcg.allObjects[i]);
-                Thread.Sleep(500);
+                Thread.Sleep(1500);
+
+				// Hearthstone shop
                 Console.Write("\n");
                 stores.hs.generateItems(stores.hs.storeItems, 1);
-                Thread.Sleep(1000);
+                Thread.Sleep(50);
                 DoTransactions(customers, stores.hs.allObjects[i]);
-                Thread.Sleep(500);
+                Thread.Sleep(1500);
             }
 		}
+		#endregion
 
+		/// <summary>
+		/// Sleep for debugging purposes.
+		/// </summary>
+		#region Sleep
 		public void Sleep()
 		{
 			System.Threading.Thread.Sleep(50);
 			Console.Write("\nPress any key to continue...");
 			Console.ReadKey();
 		}
+		#endregion
 
-        // Multithreading function. Performs the transactions.
-        private static void DoTransactions(List<Customer> customers, StoreItem storeItem)
+		/// <summary>
+		/// Method handling transaction between store and customer.
+		/// Only one customer can buy an item put up for sale
+		/// </summary>
+		/// <param name="customers"></param>
+		/// <param name="storeItem"></param>
+		#region DoTransactions
+		private static void DoTransactions(List<Customer> customers, StoreItem storeItem)
         {
             for (int i = 0; i < customers.Count; ++i)
             {
@@ -69,5 +91,6 @@ namespace Bazaar
                 threads[j].Start(storeItem);
             }
         }
+		#endregion
 	}
 }
